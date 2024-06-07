87f76527c2fa35fe983947334c2993f4f9c2b733
Carlos Febres <febres0492@gmail.com>
2024-06-07 18:05:20 -0400

    Update

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -76,20 +76,20 @@ router.post('/signup', async (req, res) => {
 })
 
 //addding item to user's favorites
-router.post('/addFavorite', async (req, res) => {
-    try {
-        const item = {
-            userId: req.session.user_id,
-            itemType: req.body.itemType,
-            itemData: req.body.itemData,
-        }
-        const fabItem = await Favorites.create(item)
-        res.status(200).json(fabItem)
+// router.post('/addFavorite', async (req, res) => {
+//     try {
+//         const item = {
+//             userId: req.session.user_id,
+//             itemType: req.body.itemType,
+//             itemData: req.body.itemData,
+//         }
+//         const fabItem = await Favorites.create(item)
+//         res.status(200).json(fabItem)
 
-    } catch (err) {
-        console.log('err', err)
-        res.status(400).json(err)
-    }
-})
+//     } catch (err) {
+//         console.log('err', err)
+//         res.status(400).json(err)
+//     }
+// })
 
 module.exports = router;
