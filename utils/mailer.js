b53be4ebeb086abe0613b873039ac38be308854b
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-06-11 17:07:46 -0700

    Sets the user and password to env variables, updates the password and creates the send token function

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,11 +1,31 @@
 const mailer = require('nodemailer');
+require('dotenv').config();
 
 
-const transport = mailer.createTransport({
-    service: "gmail",
 
-    auth:{
-        user:'favoritevault904@gmail.com',
-        pass:'FavoriteV4u1t!!'
+async function sendToken(user, token) {
+
+    const transporter = mailer.createTransport({
+        service: 'gmail', // You can use any email service provider
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        },
+    });
+    const mailOptions = {
+        from: `Favorite Vault <${process.env.EMAIL_USER}>`,
+        to: user,
+        subject: 'Password Reset',
+        text: `Please use the following token to reset your password: ${token}`,
+        html: `<p>Please use the following token to reset your password: <strong>${token}</strong></p>`,
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log('Password reset email sent.');
+    } catch (error) {
+        console.error('Error sending email:', error);
     }
-});
+}
+
+sendToken('ergordosesi@gmail.com','789101');
