bd72f0c290d7e49c7d84df7697b473a6c46fd5d9
Carlos Febres <febres0492@gmail.com>
2024-06-14 00:23:53 -0400

    Fixing loging problem

--- public/js/login.js
+++ public/js/login.js
@@ -1,25 +1,51 @@
-const loginFormHandler = async (event) => {
+const loginFormHandler = (event) => {
     event.preventDefault();
 
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const email = $('#email-login').val().trim();
+    const password = $('#password-login').val().trim();
 
     if (email && password) {
-        const response = await fetch('/api/users/login', {
+        $.ajax({
+            url: '/api/users/login',
             method: 'POST',
-            body: JSON.stringify({ email, password }),
-            headers: { 'Content-Type': 'application/json' },
+            contentType: 'application/json',
+            data: JSON.stringify({ email, password }),
+            success: function(response) {
+                document.location.replace('/');
+            },
+            error: function(xhr, status, error) {
+                console.log('loging.js');
+                showMessageInModal('Incorrect username or password');
+            }
         });
-
-        if (response.ok) {
-            document.location.replace('/');
-        } else {
-            console.log('loging.js');
-            showMessageInModal('Incorrect username or password');
-        }
     }
 };
 
-document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
+$('.login-form').on('submit', loginFormHandler);
+
+
+// const loginFormHandler = async (event) => {
+//     event.preventDefault();
+
+//     const email = document.querySelector('#email-login').value.trim();
+//     const password = document.querySelector('#password-login').value.trim();
+
+//     if (email && password) {
+//         const response = await fetch('/api/users/login', {
+//             method: 'POST',
+//             body: JSON.stringify({ email, password }),
+//             headers: { 'Content-Type': 'application/json' },
+//         });
+
+//         if (response.ok) {
+//             document.location.replace('/');
+//         } else {
+//             console.log('loging.js');
+//             showMessageInModal('Incorrect username or password');
+//         }
+//     }
+// };
+
+// document
+//     .querySelector('.login-form')
+//     .addEventListener('submit', loginFormHandler);
--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const sess = {
     cookie: {
         maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
         httpOnly: true,
-        secure: true,
+        secure: process.env.NODE_ENV == 'production',
         sameSite: 'strict',
     },
     resave: false,
