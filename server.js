d57431b4a9b56864e3863af1f6d7e9e40ffb3a30
Carlos Febres <febres0492@gmail.com>
2024-06-16 17:09:54 -0400

    Updating from temp-branch

--- server.js
+++ server.js
@@ -43,5 +43,5 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(routes)
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(helpers.c(`Now seving on http://localhost:${PORT}/`)))
+    app.listen(PORT, () => console.log(helpers.c(`Site is now live`)))
 })
